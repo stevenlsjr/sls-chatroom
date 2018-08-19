d3c71a30f94813027f7ec0bfbda583dc972d80be
Steve Shea <stevenlsjr@gmail.com>
2018-08-19 11:52:00 -0400

    added bundle-tracker

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,21 @@
 const backendUrl = process.env.VUE_APP_BACKEND_URL;
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  devServer: {
-    proxy: [{
-      context: ['/docs/', '/api/v0/', '/static/'],
-      target: backendUrl,
-      ws: true,
-      changeOrigin: true
-    }],
+  devServer : {
+    proxy : [ {
+      context : [ '/docs/', '/api/v0/', '/static/' ],
+      target : backendUrl,
+      ws : true,
+      changeOrigin : true
+    } ],
 
-    historyApiFallback: false
+    historyApiFallback : false
+  },
+  chainWebpack : config => {
+    const mode = config.get('mode');
+    config.plugin('bundle').use(BundleTracker, {
+      filename: './webpack-stats.json'
+    });
   }
 };
